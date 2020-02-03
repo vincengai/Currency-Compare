d1156e30d0a3705dcac853e8be383d934c5d6a64
Vince <53957319+vincengai@users.noreply.github.com>
2020-02-03 08:32:01 -0800

    upload to heroku

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ const pojo = () => {
       {
         cat: "Industrials",
         name: "Bitcoin",
-        value: 30,
+        value: 45,
         icon: "../img/BTC.png",
         desc: `
         3M Co. issfdbds a technology company, which manufactures industrial, safety and consumer products`,
@@ -14,7 +14,7 @@ const pojo = () => {
       {
         cat: "financials",
         name: "Ethereum",
-        value: 10,
+        value: 37,
         icon: "../img/ETH.png",
         desc: `
         American Express Co. engages in the provision of charge
@@ -25,7 +25,7 @@ const pojo = () => {
       {
         cat: "technology",
         name: "Bitcoin Cash",
-        value: 10,
+        value: 32,
         icon: "../img/BCH.png",
         desc: `
       Apple, Inc. engages in the design, manufacture, and sale of smartphones, personal 
@@ -36,7 +36,7 @@ const pojo = () => {
       {
         cat: "technology",
         name: "Binance Coin",
-        value: 10,
+        value: 30,
         icon: "../img/BNB.svg",
         desc: `
         The Boeing Co. is an aerospace company, which engages in the manufacture of 
@@ -47,7 +47,7 @@ const pojo = () => {
       {
         cat: "industrial",
         name: "Litecoin",
-        value: 10,
+        value: 16,
         icon: "../img/ltc.png",
         desc: `
         Caterpillar, Inc. engages in the manufacture of construction and mining equipment, 
@@ -58,7 +58,7 @@ const pojo = () => {
       {
         cat: "energy",
         name: "Tron",
-        value: 10,
+        value: 4,
         icon: "../img/TRX.png",
         desc: `
         Chevron Corp. engages in the provision of administrative, financial management, 
@@ -69,7 +69,7 @@ const pojo = () => {
       {
         cat: "technology",
         name: "Ripple",
-        value: 10,
+        value: 20,
         icon: "../img/XRP.png",
         desc: `
         Cisco Systems, Inc. engages in the design, manufacture, and sale of Internet 
@@ -80,7 +80,7 @@ const pojo = () => {
       {
         cat: "consumer staples",
         name: "Stellar Lumens",
-        value: 10,
+        value: 15,
         icon: "../img/XLM.png",
         desc: `
         The Coca-Cola Company is an American multinational corporation, and manufacturer, 
@@ -91,7 +91,7 @@ const pojo = () => {
       {
         cat: "consumer discretionary",
         name: "Dash",
-        value: 10,
+        value: 15,
         icon: "../img/DASH.png",
         desc: `
         The Walt Disney Company, commonly known as Disney, is an American diversified multinational mass 
@@ -112,172 +112,13 @@ const pojo = () => {
       {
         cat: "energy",
         name: "Neo",
-        value: 10,
+        value: 4,
         icon: "../img/NEO.png",
         desc: `
         Exxon Mobil Corp. engages in the exploration, development, and distribution of oil, gas, and petroleum products.`,
         link: "Here",
         linkUrl: "http://coinbase-appacademy.herokuapp.com/#/coins/NEO"
       }
-      // {
-      //   cat: "financials",
-      //   name: "Goldman Sachs",
-      //   value: 10,
-      //   icon: "../img/goldman.png",
-      //   desc: `
-      //   Goldman Sachs Group, Inc. engages in global investment banking, securities, and investment management, which provides financial services.
-      // `
-      // },
-      // {
-      //   cat: "technology",
-      //   name: "IBM",
-      //   value: 10,
-      //   icon: "../img/ibm.png",
-      //   desc: `
-      //   International Business Machines Corp. is an information technology company,
-      //   which provides integrated solutions that leverage information technology and knowledge of business processes.
-      // `
-      // },
-      // {
-      //   cat: "technology",
-      //   name: "Intel",
-      //   value: 10,
-      //   icon: "../img/intel.png",
-      //   desc: `
-      //  Intel Corp. engages in the design, manufacture, and sale of computer products and
-      //  technologies. It delivers computer, networking, data storage, and communications platforms.
-      // `
-      // },
-      // //////////////
-      // {
-      //   cat: "consumer",
-      //   name: "Johnson & Johnson",
-      //   value: 10,
-      //   icon: "../img/johnson.png",
-      //   desc: `
-      //  Johnson & Johnson is a holding company, which engages in the research and development,
-      //  manufacture and sale of products in the health care field.
-      // `
-      // },
-      // {
-      //   cat: "financials",
-      //   name: "JPMorgan Chase",
-      //   value: 10,
-      //   icon: "../img/jpmorgan.png",
-      //   desc: `
-      //  Intel Corp. engages in the design, manufacture, and sale of computer products and
-      //  technologies. It delivers computer, networking, data storage, and communications platforms.
-      // `
-      // },
-      // {
-      //   cat: "technology",
-      //   name: "McDonald",
-      //   value: 10,
-      //   icon: "../img/mcdonald.png",
-      //   desc: `
-      //  Intel Corp. engages in the design, manufacture, and sale of computer products and
-      //  technologies. It delivers computer, networking, data storage, and communications platforms.
-      // `
-      // },
-      // {
-      //   cat: "technology",
-      //   name: "Merck",
-      //   value: 10,
-      //   icon: "../img/merck.png",
-      //   desc: `
-      //  Intel Corp. engages in the design, manufacture, and sale of computer products and
-      //  technologies. It delivers computer, networking, data storage, and communications platforms.
-      // `
-      // },
-      // {
-      //   cat: "technology",
-      //   name: "Microsoft",
-      //   value: 10,
-      //   icon: "../img/microsoft.png",
-      //   desc: `
-      //  Intel Corp. engages in the design, manufacture, and sale of computer products and
-      //  technologies. It delivers computer, networking, data storage, and communications platforms.
-      // `
-      // },
-      // {
-      //   cat: "technology",
-      //   name: "Nike",
-      //   value: 10,
-      //   icon: "../img/nike.png",
-      //   desc: `
-      //  Intel Corp. engages in the design, manufacture, and sale of computer products and
-      //  technologies. It delivers computer, networking, data storage, and communications platforms.
-      // `
-      // },
-      // {
-      //   cat: "technology",
-      //   name: "Pfizer",
-      //   value: 10,
-      //   icon: "../img/pfizer.png",
-      //   desc: `
-      //  Intel Corp. engages in the design, manufacture, and sale of computer products and
-      //  technologies. It delivers computer, networking, data storage, and communications platforms.
-      // `
-      // },
-      // {
-      //   cat: "technology",
-      //   name: "Proctor & Gamble",
-      //   value: 10,
-      //   icon: "../img/gamble.png",
-      //   desc: `
-      //  Intel Corp. engages in the design, manufacture, and sale of computer products and
-      //  technologies. It delivers computer, networking, data storage, and communications platforms.
-      // `
-      // },
-      // {
-      //   cat: "financials",
-      //   name: "Travelers Companies Inc",
-      //   value: 10,
-      //   icon: "../img/travelers.png",
-      //   desc: `
-      //  The Travelers Cos., Inc. is a holding company, which engages in the provision
-      //  of commercial and personal property and casualty insurance products and services.
-      // `
-      // },
-      // {
-      //   cat: "industrials",
-      //   name: "United Technologies",
-      //   value: 10,
-      //   icon: "../img/unitedtechnologies.png",
-      //   desc: `
-      //  United Technologies Corp. engages in the provision of products and services
-      //  to the building systems and aerospace industries worldwide.
-      // `
-      // },
-      // {
-      //   cat: "healthcare",
-      //   name: "UnitedHealth",
-      //   value: 10,
-      //   icon: "../img/unitedhealth.png",
-      //   desc: `
-      //  UnitedHealth Group, Inc. engages in the provision of health care coverage, software, and data consultancy services.
-      // `
-      // },
-      // {
-      //   cat: "technology",
-      //   name: "Verizon",
-      //   value: 10,
-      //   icon: "../img/verizon.png",
-      //   desc: `
-      //  Verizon Communications, Inc. is a holding company, which engages in the provision communications,
-      //  information and entertainment products and services to consumers, businesses and governmental agencies.
-      // `
-      // },
-      // {
-      //   cat: "financials",
-      //   name: "Visa",
-      //   value: 10,
-      //   icon: "../img/visa.png",
-      //   desc: `
-      //  Visa, Inc. engages in the provision of digital payment services. It also facilitates global commerce through the transfer of value
-      //  and information among global network of consumers, merchants, financial institutions, businesses, strategic partners, and government entities.
-      // `
-      // },
       // {
       //   cat: "consumer discretionary",
       //   name: "Wal-mart",
